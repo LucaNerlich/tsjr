81f4b5d72b624973ac7f7d9d0b3cbb9741710be6
Nerlich, Luca <Luca.Nerlich@coremedia.com>
2017-04-22 10:55:28 +0200

    added package json

--- src/ShoppingCart.ts
+++ src/ShoppingCart.ts
@@ -1,20 +1,20 @@
-import {Artikel} from "./Artikel";
-import {Person} from "./Person";
+import { Artikel } from "./Artikel";
+import { Person } from "./Person";
 
 // tsc *.ts --watch
 // tsc -w -p .
 
-export interface ShoppingCart{
-    owner :Person;
-    itemsToBuy :Array<Artikel>;
-    currentPrice :number;
+export interface ShoppingCart {
+    owner: Person;
+    itemsToBuy: Artikel[];
+    currentPrice: number;
 }
 
-function getShoppingCartSum(shoppingCart :ShoppingCart){
-    let sum :number;
+function getShoppingCartSum(shoppingCart: ShoppingCart) {
+    let sum: number;
 
     // get each articles price and add to sum
-    for (let item of shoppingCart.itemsToBuy){
+    for (let item of shoppingCart.itemsToBuy) {
         sum += item.price;
     }
 
