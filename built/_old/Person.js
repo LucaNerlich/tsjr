44477d4cac7d32d9f2e801d1a2a2c01bfbcf2a98
Nerlich, Luca <Luca.Nerlich@googlemail.com>
2017-04-28 20:50:27 +0200

    JR-14 updated gitignore, removed old files

--- built/_old/Person.js
+++
@@ -1,7 +0,0 @@
-export class Person {
-    constructor(firstname, lastName) {
-        this.firstName = firstname;
-        this.lastName = lastName;
-    }
-}
-//# sourceMappingURL=Person.js.map
---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="modules/debug/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/ts-node/index.d.ts" />
+/// <reference path="modules/typescript-collections/index.d.ts" />
+/// <reference path="modules/typescript/index.d.ts" />
