1d20247f0fa9605120650c78ed0d79a82105aa1d
Nerlich, Luca <Luca.Nerlich@googlemail.com>
2017-04-28 19:59:24 +0200

    JR-14 added compiled built.js

--- built/App.js
+++ built/App.js
@@ -1,2 +1,19 @@
 "use strict";
+let canvas;
+let context;
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    context.fillStyle = "black";
+    context.fillRect(0, 0, 1280, 720);
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.lineWidth = 5;
+    context.arc(400, 400, 100, 0, 2 * Math.PI);
+    context.stroke();
+}
+window.onload = () => {
+    canvas = document.getElementById('mainGameCanvas');
+    context = canvas.getContext("2d");
+    gameLoop();
+};
 //# sourceMappingURL=App.js.map
