f49880e1fb26ac4bdf8021b777e9a75521d25f4c
Nerlich, Luca <Luca.Nerlich@googlemail.com>
2017-04-28 16:35:55 +0200

    added comments

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -1,1 +1,8 @@
-/** empty file a */
+/** 
+ * How do we display / render the 'game'?
+ * NodeJS /getGame -> code complett als js static ausliefern
+ *  html5 canvas und hier grafiken reinknallen?
+ * stand alone anwendung? kA wie das mit js geht - wenn ueberhaupt
+ * 
+ */
+
