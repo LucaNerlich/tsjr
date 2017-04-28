8bf0b8fba4c84d323eb8437cefe792d680c5e3db
Nerlich, Luca <Luca.Nerlich@googlemail.com>
2017-04-28 19:54:57 +0200

    updated app.js

--- src/ts/App.ts
+++ src/ts/App.ts
@@ -1,1 +1,21 @@
-/** main */
+/** Main Game Class */
+
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    context.fillStyle = "black";
+    context.fillRect(0, 0, 1280, 720);
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.lineWidth = 5;
+    context.arc(400, 400, 100, 0, 2 * Math.PI);
+    context.stroke();
+}
+
+window.onload = () => {
+    canvas = <HTMLCanvasElement>document.getElementById('mainGameCanvas');
+    context = canvas.getContext("2d");
+    gameLoop();
+}
