f4759a19152df263c2456adfc5b54deeb91d3e5b
Nerlich, Luca <Luca.Nerlich@googlemail.com>
2017-04-29 12:23:31 +0200

    clean up

--- src/ts/App.ts
+++ src/ts/App.ts
@@ -2,15 +2,9 @@ import Circle from './Model/Circle.js';
 
 let canvas: HTMLCanvasElement;
 let context: CanvasRenderingContext2D;
-
 let circle1: Circle;
-// let circle2: Circle = new Circle(400, 550, 150, context, "blue", 5);
-
-let x: number = 0;
 
 function gameLoop() {
-    // let start: number = new Date().getTime();
-
     requestAnimationFrame(gameLoop);
     context.fillStyle = "black";
     context.fillRect(0, 0, 1280, 720);
@@ -18,22 +12,13 @@ function gameLoop() {
     context.strokeStyle = "red";
     context.lineWidth = 5;
     context.arc(1280 / 2, 720 / 2, 100, 0, 2 * Math.PI);
+    context.stroke();
 
     circle1.draw();
 
-    context.stroke();
-
-
-    // if (circle1.x++ >= 1280 + circle1.radius) {
-    //     circle1.x = -circle1.radius;
-    // }
-
-    // if (circle2.y++ >= 720 + circle2.radius) {
-    //     circle2.y = -circle2.radius;
-    // }
-
-    // let end: number = new Date().getTime();
-    // console.log("Frame produced in: " + (end - start));
+    if (circle1.x++ >= 1280 + circle1.radius) {
+        circle1.x = -circle1.radius;
+    }
 }
 
 window.onload = () => {
@@ -42,7 +27,6 @@ window.onload = () => {
     context = canvas.getContext("2d");
     circle1 = new Circle(200, 300, 50, context);
     gameLoop();
-
 }
 
 function keyboardInput(event: KeyboardEvent) {
